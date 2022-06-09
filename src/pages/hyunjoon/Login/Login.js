74700c604f69bf0b2cc632e2eb9a1aa8769ad763
정현준 <slosh@jeonghyeonjun-ui-MacBookAir.local>
2022-06-09 17:10:13 +0900

    정현준 첫번째 커밋

--- src/pages/hyunjoon/Login/Login.js
+++ src/pages/hyunjoon/Login/Login.js
@@ -1,7 +1,78 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import './Login.scss';
 
-const Login = () => {
-  return <div>login page</div>;
-};
+function Login() {
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/main-hyunjoon');
+  };
+  const rightId = 'wjd2676@gmail.com';
+  const rightPw = '111111';
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
 
+  const rightIdPw = () => {
+    if (id == rightId && pw == rightPw) {
+      goToMain();
+    }
+  };
+  const wrongIdPw = () => {
+    if (id.indexOf('@') == -1) {
+      alert('아이디는 이메일형식으로 입력해주세요');
+      return;
+    }
+    if (id !== rightId) {
+      alert('아이디를 확인하세요');
+      return;
+    }
+
+    if (pw.length < 6) {
+      alert('비밀번호는 6글자 이상 입력해주세요');
+      return;
+    }
+    if (pw !== rightPw) {
+      alert('비밀번호 확인하세요');
+      return;
+    }
+  };
+  function consoleId(event) {
+    event.preventDefault();
+    rightIdPw();
+    wrongIdPw();
+  }
+  return (
+    <div className="login">
+      <div className="content">
+        <div id="text">westagram</div>
+        <div className="westaBox">
+          <form onSubmit={consoleId} className="loginBox">
+            <input
+              name="id"
+              value={id}
+              onChange={({ target: { value } }) => setId(value)}
+              className="ID"
+              type="text"
+              placeholder="전화번호,사용자 이름 또는 이메일"
+            />
+            <input
+              name="pw"
+              value={pw}
+              onChange={({ target: { value } }) => setPw(value)}
+              className="PW"
+              type="password"
+              placeholder="비밀번호"
+            />
+            <button>로그인</button>
+          </form>
+          <a id="forget" href="">
+            비밀번호를 잊으셨나요?
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default Login;
