e901783bd41911cd0bfb903d2e71f2f4c80bc4e9
chlwogur31 <chlwogur1995@naver.com>
2022-06-10 14:21:00 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/jaehyuk/Login/Login.js
+++ src/pages/jaehyuk/Login/Login.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Login.scss';
+// import "../../style/common.css";
+// import "./Common.css"
 
-const Login = () => {
-  return <div>login page</div>;
-};
+function Login() {
+  const navigate = useNavigate();
+  const response = 'SUCCESS';
+  const goToMain = () => {
+    if (response === 'SUCCESS') {
+      alert('환영합니다!');
+      navigate('/main');
+    } else {
+      alert('틀렸습니다.');
+    }
+  };
+
+  const [id, setId] = useState('');
+  const handleIdInput = event => {
+    setId(event.target.value);
+    console.log('id.value: ', id);
+  };
+  const [pw, setPw] = useState('');
+  const handlePwInput = event => {
+    setPw(event.target.value);
+    console.log('pw.value: ', pw);
+  };
+
+  return (
+    <div className="login">
+      <section className="loginWrapper">
+        <div className="logoWrapper">
+          <Link to="/main">
+            <h1 id="logo">Westagram</h1>
+          </Link>
+        </div>
+        <foam action="" method="post" className="loginMethod">
+          <div className="inputWrap">
+            <input
+              type="text"
+              id="id"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+              onChange={handleIdInput}
+            />
+            <input
+              type="password"
+              id="password"
+              placeholder="비밀번호"
+              onChange={handlePwInput}
+            />
+          </div>
+          <button className="btn" type="submit" onClick={goToMain}>
+            로그인
+          </button>
+        </foam>
+        <div className="passwordWrong">
+          <p>
+            <a href="#">비밀번호를 잊으셨나요?</a>
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
 
 export default Login;
--- src/pages/jaehyuk/Main/Main.js
+++ src/pages/jaehyuk/Main/Main.js
@@ -1,7 +1,285 @@
+import './Main.scss';
+import '../../style/common.css';
 import React from 'react';
 
-const Login = () => {
-  return <div>main page</div>;
-};
+function Main() {
+  return (
+    <>
+      <nav className="navigation">
+        <div className="navContainer">
+          <div className="nav1 navCommon">
+            <img
+              className="logoInstagramTxt"
+              src="images/jaehyuk/instagram.png"
+              alt="instagram_logo"
+            />
+          </div>
+          <input
+            id="searchInput"
+            type="search"
+            className="inputSearch"
+            placeholder="검색"
+          />
+          <div className="nav2 navCommon">
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+              alt="탐색"
+            />
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+              alt="하트"
+            />
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
+              alt="마이페이지"
+            />
+          </div>
+        </div>
+      </nav>
+      <main>
+        <div className="feeds">
+          <article className="articleBordering">
+            <header>
+              <div className="profileOfArticle">
+                <img
+                  className="imgProfile pic"
+                  src="images/jaehyuk/신세경_프로필.jpg"
+                  alt="sjkuksee님의 프로필 사진"
+                />
+                <span className="userID mainId pointSpan">sjkuksee</span>
+              </div>
+              <img
+                className="iconReact iconMore"
+                src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/more.png"
+                alt="more"
+              />
+            </header>
+            <div className="mainImage">
+              <img
+                src="images/jaehyuk/신세경_프로필.jpg"
+                alt="sjkuksee님의 피드 사진"
+                className="mainPic"
+              />
+            </div>
+            <div className="iconsReact">
+              <div className="iconsLeft">
+                <img
+                  id="mainHeart"
+                  className="iconReact"
+                  src="images/jaehyuk/firstHeart.png"
+                  alt="하트"
+                />
+                <img
+                  id="mainBallon"
+                  className="iconReact"
+                  src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/comment.png"
+                  alt="말풍선"
+                />
+                <img
+                  id="dmIcon"
+                  className="iconReact"
+                  src="images/jaehyuk/dm.png"
+                  alt="DM"
+                />
+              </div>
+              <img
+                id="bookMark"
+                className="iconReact"
+                src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/bookmark.png"
+                alt="북마크"
+              />
+            </div>
+            <div className="reaction">
+              <div className="likedPeople">
+                <img
+                  className="pic"
+                  src="images/jaehyuk/신세경_프로필.jpg"
+                  alt="sjkuksee님의 프로필 사진"
+                />
+                <p>
+                  <span className="pointSpan">_hyyyyyk</span>님{' '}
+                  <span className="pointSpan" />외 <span id="likeNumber" />이
+                  좋아합니다
+                </p>
+              </div>
+              <div className="description">
+                <p>
+                  <span className="pointSpan userID">sjkuksee</span>
+                  <span className="atTag">@dr.forhair_official @unove</span> 🌱
+                </p>
+              </div>
+              <div className="commentSection">
+                <ul className="comments">
+                  <li className="commentsLi">
+                    <span>
+                      <span className="pointSpan userID">vogue_official</span>
+                      화보 잘 나왔네요!!
+                    </span>
+                    <img
+                      className="commentHeart"
+                      src="images/jaehyuk/firstHeart.png"
+                      alt="하트"
+                    />
+                  </li>
+                </ul>
+                <div className="timeLog">
+                  <span className="timeLogSpan">50분 전</span>
+                </div>
+              </div>
+            </div>
+            <div className="hl" />
+            <div className="comment">
+              <input
+                id="inputComment"
+                className="inputComment"
+                type="text"
+                placeholder="댓글 달기..."
+              />
+              <button type="submit" className="submitComment" disabled>
+                게시
+              </button>
+            </div>
+          </article>
+        </div>
+        <div className="mainRight">
+          <div className="myProfile">
+            <img
+              className="pic"
+              src="images/jaehyuk/me.jpg"
+              alt="_hyyyyyk님의 프로필 사진"
+            />
+            <div className="myProfileDiv">
+              <span className="userID pointSpan">_hyyyyyk</span>
+              <span className="subSpan">JAE HYUK CHOI</span>
+            </div>
+          </div>
+          <div className="sectionStory articleBordering">
+            <div className="menuTitle">
+              <span className="subTitle">스토리</span>
+              <span className="findMore">모두 보기</span>
+            </div>
+            <ul className="storyList">
+              <li className="storyListLi">
+                <div className="gradientWrap">
+                  <img
+                    className="imgProfile story"
+                    src="images/jaehyuk/wecode.jpg"
+                    alt="wecode_bootcamp님의 프로필 사진"
+                  />
+                </div>
+                <div className="profileText">
+                  <span className="userID pointSpan">wecode_bootcamp</span>
+                  <span className="subSpan">12분 전</span>
+                </div>
+              </li>
+              <li className="storyListLi">
+                <div className="gradientwrap">
+                  <img
+                    className="imgProfile story"
+                    src="images/jaehyuk/신세경_프로필.jpg"
+                    alt="han_ye_seul님의 프로필 사진"
+                  />
+                </div>
+                <div className="profileText">
+                  <span className="userID pointSpan">han_ye_seul</span>
+                  <span className="subSpan">28분 전</span>
+                </div>
+              </li>
+              <li className="storyListLi">
+                <div className="gradientWrap">
+                  <img
+                    className="imgProfile story"
+                    src="image/신세경_프로필.jpg"
+                    alt="dntlrdl님의 프로필 사진"
+                  />
+                </div>
+                <div className="profileText">
+                  <span className="userID pointSpan">dntlrdl</span>
+                  <span className="subSpan">40분 전</span>
+                </div>
+              </li>
+              <li className="storyListLi">
+                <div className="gradientWrap">
+                  <img
+                    className="imgProfile story"
+                    src="images/jaehyuk/신세경_프로필.jpg"
+                    alt="i_icaruswalks님의 프로필 사진"
+                  />
+                </div>
+                <div className="profileText">
+                  <span className="userID pointSpan">i_icaruswalks</span>
+                  <span className="subSpan">56분 전</span>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="sectionRecommend articleBordering">
+            <div className="menuTitle">
+              <span className="subTitle">회원님을 위한 추천</span>
+              <span className="findMore">모두 보기</span>
+            </div>
+            <ul className="recommendList">
+              <li className="recommendListLi">
+                <div className="recommendFriendProfile">
+                  <img
+                    className="imgProfile"
+                    src="images/jaehyuk/신세경_프로필.jpg"
+                    alt="renebaebae님의 프로필 사진"
+                  />
+                  <div className="profileText">
+                    <span className="userID pointSpan">renebaebae</span>
+                    <span className="subSpan">
+                      hi_sseulgi님 외 2명이 팔로우합니다
+                    </span>
+                  </div>
+                </div>
+                <span className="btnFollow">팔로우</span>
+              </li>
+              <li className="recommendListLi">
+                <div className="recommendFriendProfile">
+                  <img
+                    className="imgProfile"
+                    src="images/jaehyuk/신세경_프로필.jpg"
+                    alt="신세경 이미지"
+                  />
+                  <div className="profileText">
+                    <span className="userID pointSpan">_jeongjaehyun</span>
+                    <span className="subSpan">_hyyyyyk님이 팔로우합니다</span>
+                  </div>
+                </div>
+                <span className="btnFollow">팔로우</span>
+              </li>
+              <li className="recommendListLi">
+                <div className="recommendFriendProfile">
+                  <img
+                    className="imgProfile"
+                    src="images/jaehyuk/me.jpg"
+                    alt="leehi_hi님의 프로필 사진"
+                  />
+                  <div className="profileText">
+                    <span className="userID pointSpan">leehi_hi</span>
+                    <span className="subSpan">
+                      dwdqaadf님 외 5명이 팔로우합...
+                    </span>
+                  </div>
+                </div>
+                <span className="btnFollow">팔로우</span>
+              </li>
+            </ul>
+          </div>
+          <footer className="footer">
+            <p className="instaScript">
+              소개 ∙ 도움말 ∙ 홍보 센터 ∙ API ∙ 채용 정보 ∙ 개인정보처리방침 ∙{' '}
+              <br />
+              약관 ∙ 위치 ∙ 언어
+              <br />
+              <br />© 2022 INSTAGRAM FROM META
+            </p>
+          </footer>
+        </div>
+      </main>
+    </>
+  );
+}
 
-export default Login;
+export default Main;
